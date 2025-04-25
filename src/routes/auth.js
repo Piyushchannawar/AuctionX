97161404df4a140e037cc058ad047cd7df8de9c5
Piyush Channawar <channawarpiyush@gmail.com>
2025-04-25 20:19:36 +0530

    feat(auth): add validation schema for user registration

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const authController = require('../controllers/auth/index')
+const authController = require('../controllers/auth/index');
+const  validateSchema  = require('../middlewares/validator');
+const registerSchema = require('../middlewares/validationSchema/registerUser');
 const authRouter = express.Router();
 
 
-authRouter.post('/register', authController.registerUser);
+authRouter.post('/register',validateSchema(registerSchema), authController.registerUser);
 
 
 
